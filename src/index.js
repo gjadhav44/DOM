1884ce21e06ba33a65a2377af114510a6d8f9623
gjadhav44 <57444523+gjadhav44@users.noreply.github.com>
2023-03-27 00:35:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,27 +15,15 @@ function addItem(e) {
 
   // Get input value
   var newItem = document.getElementById("item").value;
-
+  var discription = document.getElementById("discription").value;
   // Create new li element
   var li = document.createElement("li");
   // Add class
   li.className = "list-group-item";
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" " + discription));
   // Create del button element
-  var editBtn = document.createElement("button");
-
-  // Add classes to del button
-  editBtn.className = "btn btn-danger btn-sm float-right delete";
-
-  // Append text node
-  editBtn.appendChild(document.createTextNode("Edit"));
-
-  // Append button to li
-  li.appendChild(editBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
 
   // Create del button element
   var deleteBtn = document.createElement("button");
@@ -48,6 +36,19 @@ function addItem(e) {
 
   // Append button to li
   li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+  var editBtn = document.createElement("button");
+
+  // Add classes to del button
+  editBtn.className = "btn btn-normal btn-sm float-right delete";
+
+  // Append text node
+  editBtn.appendChild(document.createTextNode("Edit"));
+
+  // Append button to li
+  li.appendChild(editBtn);
 
   // Append li to list
   itemList.appendChild(li);
