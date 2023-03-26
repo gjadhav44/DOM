abc0c4c2d8599abe8491e6da62a3238c04cca531
gjadhav44 <57444523+gjadhav44@users.noreply.github.com>
2023-03-26 22:36:24 +0530

    dom4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+
+import "./styles.css";
 var header_title = document.getElementById("header-title");
 var header = document.getElementById("main-header");
 console.log(header_title);
@@ -12,4 +14,11 @@ list[2].style.backgroundColor = "Yellow";
 for (var i = 0; i < 4; i++) {
   list[i].style.color = "blue";
   list[i].style.fontWeight = "bold";
+  list[i].style.backgroundColor = "#ccc";
+}
+var li5 = document.getElementsByTagName("li");
+for (var i = 0; i < 4; i++) {
+  li5[4].style.color = "blue";
+  li5[4].style.fontWeight = "bold";
+  li5[4].style.backgroundColor = "#ccc";
 }
